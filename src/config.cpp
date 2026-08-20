1d2ff3f793faf768843cf2d9e798224e7ed051f5
Davide Cacopardi <davidecacopardi@gmail.com>
2026-08-20 13:41:17 +0200

    fixed config.cpp again

--- src/config.cpp
+++ src/config.cpp
@@ -89,7 +89,7 @@ runConfig loadConfig(const std::string& filepath) {
         cfg.LJ_request_Ekin      = j["monte_carlo"].value("LJ_request_Ekin", cfg.LJ_request_Ekin);
 
         if (j["monte_carlo"].contains("nelderMead_init")) {
-            cfg.nelderMead_init = j["physics"]["nelderMead_init"].get<std::vector<double>>();
+            cfg.nelderMead_init = j["monte_carlo"]["nelderMead_init"].get<std::vector<double>>();
         }
     }
 
